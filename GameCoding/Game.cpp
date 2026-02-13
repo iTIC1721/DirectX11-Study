5cff98de86582369c9e221a36fa386b0805592d7
iTIC <paul2314@naver.com>
2026-02-13 12:42:31 +0900

    [Lec24] GameObject
    여러 오브젝트를 범용적으로 관리하기 위한 GameObject 클래스 추가

--- GameCoding/Game.cpp
+++ GameCoding/Game.cpp
@@ -14,56 +14,16 @@ void Game::Init(HWND hwnd)
 	_hwnd = hwnd;
 
 	_graphics = make_shared<Graphics>(_hwnd);
-
-	_vertexBuffer = make_shared<VertexBuffer>(_graphics->GetDevice());
-	_indexBuffer = make_shared<IndexBuffer>(_graphics->GetDevice());
-	_inputLayout = make_shared<InputLayout>(_graphics->GetDevice());
-	_geometry = make_shared<Geometry<VertexTextureData>>();
-	_vertexShader = make_shared<VertexShader>(_graphics->GetDevice());
-	_pixelShader = make_shared<PixelShader>(_graphics->GetDevice());
-	_constantBuffer = make_shared<ConstantBuffer<TransformData>>(_graphics->GetDevice(), _graphics->GetDeviceContext());
-	_texture = make_shared<Texture>(_graphics->GetDevice());
-	_rasterizerState = make_shared<RasterizerState>(_graphics->GetDevice());
-	_samplerState = make_shared<SamplerState>(_graphics->GetDevice());
-	_blendState = make_shared<BlendState>(_graphics->GetDevice());
-
 	_pipeline = make_shared<Pipeline>(_graphics->GetDeviceContext());
 
-	// Vertex Data
-	GeometryHelper::CreateRectangle(_geometry);
-	// Vertex Buffer
-	_vertexBuffer->Create(_geometry->GetVertices());
-	// Index Buffer
-	_indexBuffer->Create(_geometry->GetIndices());
-
-	_vertexShader->Create(L"Default.hlsl", "VS", "vs_5_0");
-	_inputLayout->Create(VertexTextureData::descs, _vertexShader->GetBlob());
-	_pixelShader->Create(L"Default.hlsl", "PS", "ps_5_0");
-
-	_rasterizerState->Create();
-	_samplerState->Create();
-	_blendState->Create();
-
-	_texture->Create(L"miku.png");
-	_constantBuffer->Create();
+	// GameObject
+	_gameObject = make_shared<GameObject>(_graphics->GetDevice(), _graphics->GetDeviceContext());
 }
 
 void Game::Update()
 {
-	// SRT
-	_localPosition.x += 0.001f;
-
-	Matrix matScale = Matrix::CreateScale(_localScale);
-	Matrix matRotation = Matrix::CreateRotationX(_localRotation.x);
-	matRotation *= Matrix::CreateRotationY(_localRotation.y);
-	matRotation *= Matrix::CreateRotationZ(_localRotation.z);
-	Matrix matTranslation = Matrix::CreateTranslation(_localPosition);
-
-	Matrix matWorld = matScale * matRotation * matTranslation;
-	_transformData.matWorld = matWorld;
-	
-	// TransformData를 넣기
-	_constantBuffer->CopyData(_transformData);
+	// 오브젝트 Update
+	_gameObject->Update();
 }
 
 void Game::Render()
@@ -72,21 +32,7 @@ void Game::Render()
 
 	// 오브젝트 그리기
 	{
-		PipelineInfo info;
-		info.inputLayout = _inputLayout;
-		info.vertexShader = _vertexShader;
-		info.pixelShader = _pixelShader;
-		info.rasterizerState = _rasterizerState;
-		info.blendState = _blendState;
-
-		_pipeline->UpdatePipeline(info);
-
-		_pipeline->SetVertexBuffer(_vertexBuffer);
-		_pipeline->SetIndexBuffer(_indexBuffer);
-		_pipeline->SetConstantBuffer(0, SS_VertexShader, _constantBuffer);
-		_pipeline->SetTexture(0, SS_PixelShader, _texture);
-
-		_pipeline->DrawIndexed(_geometry->GetIndexCount(), 0, 0);
+		_gameObject->Render(_pipeline);
 	}
 
 	_graphics->RenderEnd();
