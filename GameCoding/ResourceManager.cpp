0227af67966c6871417c50a3cd6d72f32da44d1e
iTIC <paul2314@naver.com>
2026-02-26 15:22:27 +0900

    [Lec34] Data
    tinyxml2을 이용해 애니메이션 데이터를 XML로 저장 및 로드

--- GameCoding/ResourceManager.cpp
+++ GameCoding/ResourceManager.cpp
@@ -90,5 +90,9 @@ void ResourceManager::CreateDefaultAnimation()
 
 	Add(animation->GetName(), animation);
 
-	// XML
+	// XML / JSON
+	animation->Save(L"TestAnim.xml");
+
+	//shared_ptr<Animation> anim2 = make_shared<Animation>();
+	//anim2->Load(L"TestAnim.xml");
 }
